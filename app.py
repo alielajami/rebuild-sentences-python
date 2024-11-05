5b726c799bd435afed88c1a807cd5efc652317c7
alielajami <alielajami@outlook.com>
2024-11-05 16:55:58 +0200

    creating the function to modify the words and join them in a sentence

--- app.py
+++ app.py
@@ -10,4 +10,14 @@ for j in range(len(words_list)):
     num = int(input(f"Enter the number of letters in word {j+1}: "))
     numbers_list.append(num)
 
-print(words_list, numbers_list)
+def rebuild_sentence(words_array, numbers_array):
+    '''' Function to rebuild the sentence from the list of words '''
+    modified_words_list = []
+    for k in range(len(words_array)):
+        modified_word = words_array[k][:numbers_array[k]]
+        modified_words_list.append(modified_word)
+
+    result_sentence = ' '.join(modified_words_list)
+    return result_sentence
+
+print("Reconstructed sentence:", rebuild_sentence(words_list, numbers_list))
